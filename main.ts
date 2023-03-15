20fee8953628e3b6a10bde16facf56430c478b31
ASHISH YADAV <ashish.yd@gmail.com>
2023-03-15 10:19:46 +0530

    working checkpoint commit

--- main.ts
+++ main.ts
@@ -3,17 +3,19 @@ import { App, TerraformStack } from 'cdktf';
 import { HashicupsProvider } from './.gen/providers/hashicups/provider';
 import { Order, OrderConfig } from './.gen/providers/hashicups/order';
 import { DataHashicupsOrder } from './.gen/providers/hashicups/data-hashicups-order';
-import { DataHashicupsCoffees } from './.gen/providers/hashicups/data-hashicups-coffees';
-import { DataHashicupsIngredients } from './.gen/providers/hashicups/data-hashicups-ingredients';
+// import { DataHashicupsCoffees } from './.gen/providers/hashicups/data-hashicups-coffees';
+// import { DataHashicupsIngredients } from './.gen/providers/hashicups/data-hashicups-ingredients';
+// import { orderItemsToTerraform } from './.gen/providers/hashicups/order/index';
+
 class HashicupsCrudApi extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
     // define resources here
     const hashicupsProvider = new HashicupsProvider(this, 'hushicups', {
-      username: 'admin',
-      password: 'admin',
-      host: 'https://localhost:19090',
+      username: 'education',
+      password: 'test123',
+      host: 'http://localhost:19090',
     });
 
     const orderItems: OrderConfig = {
@@ -33,28 +35,30 @@ class HashicupsCrudApi extends TerraformStack {
       ],
     };
 
-    const order = new Order(this, 'order', {
+    const orderData = new DataHashicupsOrder(this, 'order-data', {
       provider: hashicupsProvider,
-      lastUpdated: new Date().toISOString(),
+      id: 1,
+    });
+
+    new Order(this, 'order', {
+      provider: hashicupsProvider,
       ...orderItems,
+      dependsOn: [orderData],
     });
 
-    new DataHashicupsOrder(this, 'getorder', {
-      provider: hashicupsProvider,
-      id: +order.id,
-    });
+    // const coffee = new DataHashicupsCoffees(this, 'coffees', {
+    //   provider: hashicupsProvider,
+    //   dependsOn: [order],
+    // });
 
-    const coffeeData = new DataHashicupsCoffees(this, 'coffees', {
-      provider: hashicupsProvider,
-    });
-
-    new DataHashicupsIngredients(this, 'ingredients', {
-      provider: hashicupsProvider,
-      coffeeId: +coffeeData.id,
-    });
+    // new DataHashicupsIngredients(this, 'ingredients', {
+    //   provider: hashicupsProvider,
+    //   coffeeId: order.items.get(0).coffee.id,
+    //   dependsOn: [coffee],
+    // });
   }
 }
 
 const app = new App();
-new HashicupsCrudApi(app, 'typescript-hushicups');
+new HashicupsCrudApi(app, 'typescript-hashicups');
 app.synth();
